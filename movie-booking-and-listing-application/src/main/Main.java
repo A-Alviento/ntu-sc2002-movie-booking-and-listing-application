a5a6e805a0112bd26cb515c247302e981213641d
Min <mk43v3r@gmail.com>
2022-10-26 17:34:51 +0800

    improved project folder structure

---
+++ movie-booking-and-listing-application/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/*
+ * Main entry point into the program.
+*/
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
