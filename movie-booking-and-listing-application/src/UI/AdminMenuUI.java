c01f47d496c07e468b450379084d9a6210e5d507
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2022-10-28 17:06:05 +0800

    added some UI elements

---
+++ movie-booking-and-listing-application/src/UI/AdminMenuUI.java
@@ -0,0 +1,30 @@
+package UI;
+
+import java.util.Scanner;
+
+public class AdminMenuUI {
+    
+    static Scanner sc = new Scanner(System.in);
+    private MainController mC;
+    
+    public void displayAdminUI() {
+        int selection;
+        
+        do {
+            // list out main menu selection UI
+            System.out.println("**************************************************************************************************");
+            System.out.println("1. Create Movie Listing.\n"
+                    + "2. Update/Remove Movie Listing.\n"
+                    + "3. Create Cinema Showtime.\n"
+                    + "4. Update/Remove Cinema Showtime.\n"
+                    + "5. Configure system settings.\n"
+                    + "6. Logout.");
+            System.out.println("**************************************************************************************************");
+            System.out.println("**************************************************************************************************");
+            System.out.println("\nPlease choose an option from the menu:");
+            selection = sc.nextInt();
+        }while(mC.Admin(selection) == -1); 
+        
+    }
+    
+}
---
+++ movie-booking-and-listing-application/src/UI/MainController.java
@@ -0,0 +1,6 @@
+package UI;
+
+public class MainController {
+
+    public void display 
+}
