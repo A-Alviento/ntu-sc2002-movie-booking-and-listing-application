e2c281972028af3d7938a9d1eb49772bc786fac8
Min <mk43v3r@gmail.com>
2022-10-29 17:37:29 +0800

    Changed UI package to lowercase(2).

---
+++ movie-booking-and-listing-application/src/ui/MovieUI.java
@@ -0,0 +1,29 @@
+package ui;
+
+import java.util.Scanner;
+
+public class MovieUI {
+    
+    static Scanner sc = new Scanner(System.in);
+    private MainController mC;
+    
+    public void displayMovieUI() {
+        int selection;
+        
+        do {
+            // list out main menu selection UI
+            System.out.println("**************************************************************************************************");
+            System.out.println("1. View details.\n"
+                    + "2. Seat Availability.\n"
+                    + "3. Book ticket.\n"
+                    + "4. Back.");
+            System.out.println("**************************************************************************************************");
+            System.out.println("**************************************************************************************************");
+            System.out.println("\nPlease choose an option from the menu:");
+            selection = sc.nextInt();
+        }while(mC.Movie(selection) == -1); 
+        
+    }
+    
+    
+}
---
+++ movie-booking-and-listing-application/src/ui/UserMenuUI.java
@@ -0,0 +1,30 @@
+package ui;
+
+import java.util.Scanner;
+
+public class UserMenuUI {
+    
+    static Scanner sc = new Scanner(System.in);
+    private MainController mC;
+    
+    public void displayMainUI() {
+        int selection;
+        
+        do {
+            // list out main menu selection UI
+            System.out.println("**************************************************************************************************");
+            System.out.println("1. List movies.\n"
+                    + "2. Search for a specific movie.\n"
+                    + "3. View booking history \n"
+                    + "4. List top 5 ranking by sales"
+                    + "5. List top 5 ranking by ratings"
+                    + "6. Logout.");
+            System.out.println("**************************************************************************************************");
+            System.out.println("**************************************************************************************************");
+            System.out.println("\nPlease choose an option from the menu:");
+            selection = sc.nextInt();
+        }while(mC.User(selection) == -1); 
+        
+    }
+    
+}
