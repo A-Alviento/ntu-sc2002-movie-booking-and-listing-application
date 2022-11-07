4d858ea8b6ad2ab7aa911a73be0ff606e1776d3a
LuminoFox <36134481+LuminoFox@users.noreply.github.com>
2022-11-07 17:00:43 +0800

    Add files via upload

---
+++ movie-booking-and-listing-application/src/model/CheckDate.java
@@ -0,0 +1,37 @@
+/*
+ *  A utility function used by the Price Class to check if a date is public holiday or weekend
+ *  to determine the pricing.
+ */
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class CheckDate {
+	/*
+	 * Check if a date is weekend
+	 * @param date Date to be checked
+	 * @return a boolean that shows if the date is a weekend
+	 */
+	public static boolean isWeekend(LocalDate date) {
+		if(date.getDayOfWeek() == DayOfWeek.SATURDAY ||date.getDayOfWeek() == DayOfWeek.SUNDAY ) {
+			return true;
+		}
+		return false;
+	}
+	
+	/*
+	 * Check if a date is a public holiday
+	 * @param date Date to be checked
+	 * @return a boolean that shows if the date is a public holiday
+	 */
+	public static boolean isPublicHoliday(LocalDate date) {
+		ArrayList<LocalDate> publicHolidayList = PublicHolidays.getPublicHolidays();
+		return publicHolidayList.contains(date);
+	}
+	
+	/* Note:
+	** LocalDate.of(2022,11,3) is how to create a LocalDate
+	** Alternatively, LocalDate.parse("2022-11-3") also works
+	*/
+}
---
+++ movie-booking-and-listing-application/src/model/PublicHolidays.java
@@ -0,0 +1,30 @@
+
+/*
+ * Represents list of Public Holidays to be stored in a database
+ */
+import java.util.ArrayList;
+import java.time.LocalDate;
+
+public class PublicHolidays extends Model {
+	/*
+	 * Array of the public holidays
+	 */
+	private static ArrayList<LocalDate> publicHolidayList = new ArrayList<LocalDate>();
+	
+	/*
+	 * Add a date into the list of public Holiday
+	 * @param date date to be added to the list
+	 */
+	public static void addPublicHoliday(LocalDate date) {
+		publicHolidayList.add(date);
+	}
+	
+	/*
+	 * Get List of public holiday
+	 * @return publicHolidayList  list of public holiday in db
+	 */
+	public static ArrayList<LocalDate> getPublicHolidays(){
+		return publicHolidayList;
+	}
+	
+}
