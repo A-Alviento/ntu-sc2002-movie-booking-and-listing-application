80f2c2d3bace0e24f6cd95053ef53552925b7b80
LuminoFox <36134481+LuminoFox@users.noreply.github.com>
2022-11-09 05:42:11 +0800

    Add files via upload

---
+++ movie-booking-and-listing-application/src/ui/CheckUserInput.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public final class CheckUserInput {
+	private static Scanner sc = new Scanner(System.in); 
+	
+	private static void withinRangeDouble(double min, double max, double input) {
+		if(input < min || input > max) {
+			throw new IllegalArgumentException("Enter an input between "+ min +" and "+ max);
+		}
+	}
+	
+	public static double loopUntilValidDouble(String str, double min, double max) {
+		double input = 0.0;
+		while(true) {
+			try {
+				System.out.print(str);
+				input = sc.nextDouble();
+				withinRangeDouble(min,max,input);
+				break;
+			}catch(InputMismatchException e){
+				System.out.println("You did not enter a input of type double.\n" );
+				sc.nextLine();
+			}catch(IllegalArgumentException ex) {
+				System.err.println(ex);
+				System.out.println("");
+			}
+		}
+		return input;
+	}
+	
+	private static void withinRangeInt(int min, int max, int input) {
+		if(input < min || input > max) {
+			throw new IllegalArgumentException("Enter an input between "+ min +" and "+ max);
+		}
+	}
+	public static int loopUntilValidInt(String str, int min, int max) {
+		int input = 0;
+		while(true) {
+			try {
+				System.out.print(str);
+				input = sc.nextInt();
+				withinRangeInt(min,max,input);
+				break;
+			}catch(InputMismatchException e){
+				System.out.println("You did not enter a input of type Int.\n" );
+				sc.nextLine();
+			}catch(IllegalArgumentException ex) {
+				System.err.println(ex);
+				System.out.println("");
+			}
+		}
+		return input;
+	}
+}
